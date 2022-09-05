e7144c16eb1b54dc5a26d5e5300dfae1558e6310
Christian A <christiana@iMac-de-Christian.local>
2022-09-05 14:58:23 -0500

    DEPLOY to vercel

--- api/components/user/controller.js
+++ api/components/user/controller.js
@@ -5,9 +5,9 @@ const TABLA = 'user';
 
 module.exports = function (injectedStore) {
   let store = injectedStore;
-  // if (!store) {
-  //   store = require('../../../store/dummy');
-  // }
+  if (!store) {
+    store = require('../../../store/remote-mysql');
+  }
   function list() {
     return store.list(TABLA);
   }
