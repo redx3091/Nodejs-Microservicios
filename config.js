f676b6b3a9430b0a03f445ac1fa7b9d79d83df55
Christian A <christiana@iMac-de-Christian.local>
2022-09-06 10:15:49 -0500

    ADD envirioment to config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
     secret: process.env.JWT_SECRET || 'H+KbPeShVmYq3t6w9z$C&F)J@NcQfTjW',
   },
   mysql: {
-    host: process.env.MYSQL_HOST || 'remotemysql.com',
+    host: process.env.MYSQL_HOST || '',
     user: process.env.MYSQL_USER || 'NJvMct1uEp',
     password: process.env.MYSQL_PASS || '3MzLaiuqqf',
     database: process.env.MYSQL_DB || 'NJvMct1uEp',
